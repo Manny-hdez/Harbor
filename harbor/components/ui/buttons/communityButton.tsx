c444ffcade6674cd574ee2c0c63e31a98b01d6b2
John Carraher <jkcarraher@gmail.com>
2025-04-20 01:00:12 -0700

    Working Header

--- harbor/components/ui/buttons/communityButton.tsx
+++ harbor/components/ui/buttons/communityButton.tsx
@@ -9,10 +9,10 @@ interface CommunityButtonProps {
 
 const CommunityButton: React.FC<CommunityButtonProps> = ({ onPress, count }) => {
     return (
-    <Pressable onPress={onPress} className="bg-maroon-800 rounded-md py-2 px-4 flex flex-row items-center w-100 h-10">
+    <Pressable onPress={onPress} className="bg-red-800 py-2 px-3 flex flex-row items-center w-100 h-10">
       <CommunityIcon/>
 
-      <Text className="text-white font-semibold mr-1">Community</Text>
+      <Text className="ml-2  text-white font-semibold mr-1">Community</Text>
       {count !== undefined && (
         <Text className="text-white font-semibold">({count})</Text>
       )}
