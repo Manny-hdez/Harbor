cc5e577c3318febd519e61d0b76412f869b4880b
John Carraher <jkcarraher@gmail.com>
2025-04-19 20:41:31 -0700

    Hopefully working nativewind config

--- harbor/babel.config.js
+++ harbor/babel.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'], // Add NativeWind Babel plugin
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
 };
